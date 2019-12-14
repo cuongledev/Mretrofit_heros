fb466094bcc31dd818a725f56cac4269c851a70c
cuongle <cuongle.dev@gmail.com>
2019-12-14 16:51:16 +0700

    Using retrofit call api list heroes

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mretrofit_heros
---
+++ app/src/main/java/com/hstc/lengoccuong/mretrofit/Api.java
@@ -0,0 +1,15 @@
+package com.hstc.lengoccuong.mretrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+
+
+    String BASE_URL = "https://www.simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
